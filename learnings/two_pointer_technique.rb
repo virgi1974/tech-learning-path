b3a8cab659c66d9eddd1b01a7023a879d9d2995f
virgilio1974 <vitrgilio1974@gmail.com>
2024-10-28 09:28:03 +0100

    wip

--- learnings/two_pointer_technique.rb
+++ learnings/two_pointer_technique.rb
@@ -10,3 +10,31 @@ Time Complexity:
 O(n) -> we mat loop several times but O(n) + O(n) = O(n)
 Space Complexity:
 O(1) -> modification in-place
+
+Explanation of the Two-Pointer Technique:
+Two Pointers: This approach involves using two indices (pointers) to traverse the array. In this specific problem, the two pointers are referred to as uniqueIndex and currentIndex.
+
+uniqueIndex: This pointer is used to track the position of the last unique element found in the array.
+currentIndex: This pointer is used to traverse the entire array and identify new unique elements.
+In-Place Modification: The array is modified in-place, meaning no additional space proportional to the size of the input is used. This keeps the space complexity to
+
+Maintaining Order: The order of unique elements is preserved, which is important for certain applications.
+
+Steps Involved:
+Initialization: Start by initializing both pointers at the beginning of the array.
+Traversal: Use the currentIndex to traverse the array. For each element, compare it with the element at uniqueIndex.
+Update: If a new unique element is found (nums[currentIndex] != nums[uniqueIndex]), increment uniqueIndex and update nums[uniqueIndex] with the new unique element.
+Post-Processing: After the traversal, fill the remaining positions in the array with a placeholder (e.g., "_").
+This approach ensures that the algorithm runs in linear time (
+O(n)) with constant space complexity
+O(1) making it efficient and scalable.
+
+Applications
+The two-pointer technique is widely used in various array and string problems, such as:
+
+- Removing duplicates from sorted arrays.
+- Partitioning arrays.
+- Finding pairs or triplets that satisfy certain conditions (e.g., two-sum, three-sum problems).
+- Merging two sorted arrays.
+- Solving problems involving subarrays or subsequences.
+- This technique is highly versatile and forms the basis for many optimized solutions in competitive programming and coding interviews.
--- notes.rb
+++ notes.rb
@@ -1,4 +1,5 @@
 # ITERATING OVER AN ARRAY IN REVERSE ORDER
+#.reverse_each
 
 nums = [1, 2, 3, 4, 5]
 
@@ -14,6 +15,10 @@ if k > size
   k = k % size
 end
 
+# ITERATE AN ARRAY FROM THE SECOND ELEMENT
+1.upto(array_size - 1) do |current_index|
+end
+
 # shifth & unshift
 they can easily lead to O(n^2) time complexity if used in a loop
 because they shift all elements to the right or left by one position
@@ -22,5 +27,21 @@ but if you need to use them in a loop, it is better to use a different data stru
 like a linked list or a deque
 
 # ruby methods using an in-place algorithm
-[1,2,3,4].rotate!(-k)
-[1,2,3,4].reverse!
+  [1,2,3,4].rotate!(-k)
+  [1,2,3,4].reverse!
+
+# XOR operator - find unique element in an array
+  1 ^ 1 => 0
+  1 ^ 0 => 1
+  0 ^ 1 => 1
+  0 ^ 0 => 0
+
+  [4,1,2,1,2]
+  4 ^ 1 => 5
+  5 ^ 2 => 7
+  7 ^ 1 => 6
+  6 ^ 2 => 4 #!!!! at the end we get the single number
+
+# TALLY METHOD - to count repetitions of elements in an array
+  [1,2,3,1].tally
+=> {1=>2, 2=>1, 3=>1}
